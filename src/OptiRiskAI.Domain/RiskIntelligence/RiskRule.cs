a7d11feaba106dc397de697c82a7a3a82fbb766a
Ömer Faruk YAZICI <mrfarukyazici@gmail.com>
2026-08-30 19:25:43 +0300

    feat: add RiskRule and RiskTelemetry domain entities with DDD encapsulation

---
+++ src/OptiRiskAI.Domain/RiskIntelligence/RiskRule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Entities.Auditing;
+
+
+namespace OptiRiskAI.RiskIntelligence
+{
+    public class RiskRule : FullAutidedAggregateRoot<Guid>
+    {
+        public string RuleName { get; private set; }
+
+        // Yapay zekanın ürettiği koşul (Örn: "Mesafe < 10 VE Ruzgar > 40")
+        public string ConditionExpression { get; private set; }
+
+        // Bu kural eşleştiğinde taban prime uygulanacak çarpan (Örn: 2.5)
+        public decimal RiskMultiplier { get; private set; }
+
+        // Yapay zekanın bu kuralı neden ürettiğine dair gerekçesi
+        public string AiRationale { get; private set; }
+
+        public bool IsActive { get; private set; }
+
+        // Entity Framework Core'un reflection yapabilmesi için boş constructor (DDD kuralı)
+        private RiskRule()
+        {
+        }
+
+        public RiskRule
+            (
+            Guid id,
+            string ruleName, 
+            string conditionExpression, 
+            decimal riskMultiplier, 
+            string aiRationale
+            ) 
+        {
+            RuleName = ruleName;
+            ConditionExpression = conditionExpression;
+            RiskMultiplier = riskMultiplier;
+            AiRationale = aiRationale;
+            IsActive = true;
+        }
+
+       
+
+        // Kuralın çarpanını veya gerekçesini güncellemek için davranışsal metod 
+        public void UpdateRule(decimal newMultiplier, string newRationale)
+        {
+            RiskMultiplier = newMultiplier;
+            AiRationale = newRationale;
+        }
+
+       
+        public void Deactivate()
+        {
+            IsActive = false;
+        }
+
+        public void Activate()
+        {
+            IsActive = true;
+        }
+
+
+
+
+    }
+}
---
+++ src/OptiRiskAI.Domain/RiskIntelligence/RiskTelemetry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace OptiRiskAI.RiskIntelligence
+{
+    public class RiskTelemetry : FullAuditedAggregateRoot<Guid>
+    {
+        // Haritada (Leaflet/Mapbox) göstereceğimiz koordinatlar
+        public double Latitude { get; private set; }
+        public double Latitude { get; private set; }
+
+        // Çevresel Sensör / Gözlem Verileri
+        public double DistanceToPowerLineMeters { get; private set; }
+        public double WindSpeedKmh { get; private set; }
+        public double SlopePercentage { get; private set; }
+        public string VegetationType { get; private set; } // Örn: "Kızılçam", "Maki", "Seyrek Ot"
+
+        // Verileri yapay zekanın Kural Matrisi ile eşleştirdiğinde burası dolacak
+        public Guid? AppliedRiskRuleId { get; private set; }
+        public decimal? CalculatedRiskMultiplier { get; private set; }
+        public bool IsProcessed { get; private set; }
+
+        private RiskTelemetry()
+        {
+        }
+        public RiskTelemetry(
+            Guid id,
+            double latitude,
+            double longitude,
+            double distanceToPowerLineMeters,
+            double windSpeedKmh,
+            double slopePercentage,
+            string vegetationType)
+            : base(id)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            DistanceToPowerLineMeters = distanceToPowerLineMeters;
+            WindSpeedKmh = windSpeedKmh;
+            SlopePercentage = slopePercentage;
+            VegetationType = vegetationType;
+            IsProcessed = false;
+        }
+
+        
+        public void ApplyDeterminedRisk(Guid ruleId, decimal multiplier)
+        {
+            AppliedRiskRuleId = ruleId;
+            CalculatedRiskMultiplier = multiplier;
+            IsProcessed = true;
+        }
+
+    }
+}
